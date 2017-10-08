7fe5c8e7620c2f92d11a092fd3bf546af4c37021
YeahMama <wallon.xu@lcdlv.fr>
2017-10-08 18:03:47 +0200

    Refactor: Change order of 'makeDrinks' function in order to have a better readability

--- CoffeeMachine/src/main/java/Logic.java
+++ CoffeeMachine/src/main/java/Logic.java
@@ -2,6 +2,14 @@ public class Logic {
     public final static String STICK = "0";
     public final static String MESSAGE_CODE = "M";
 
+    public String makeDrink(Drink drink, int nbOfSugars, double amount) {
+        if(amount >= drink.getPrice()) {
+            return parseToDrinkMakerProtocol(drink, nbOfSugars);
+        }
+
+        return "Need " + String.valueOf(drink.getPrice() - amount);
+    }
+
     public String parseToCustomer(String commandFromDrinkMaker) {
         String[] commands = commandFromDrinkMaker.split(":");
 
@@ -28,13 +36,5 @@ public class Logic {
 
     private boolean hasSugars(int nbOfSugars) {
         return nbOfSugars > 0;
-    }
-
-    public String makeDrink(Drink drink, int nbOfSugars, double amountOfMoneyGiven) {
-        if(amountOfMoneyGiven >= drink.getPrice()) {
-            return parseToDrinkMakerProtocol(drink, nbOfSugars);
-        }
-
-        return "Need " + String.valueOf(drink.getPrice() - amountOfMoneyGiven);
     }
 }
