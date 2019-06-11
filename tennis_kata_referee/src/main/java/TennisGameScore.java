698a53b183529986f90ed205a272556e430c3232
YeahMama <>
2019-06-11 20:56:55 +0200

    Test on converting score to tennis score

--- tennis_kata_referee/src/main/java/TennisGameScore.java
+++ tennis_kata_referee/src/main/java/TennisGameScore.java
@@ -53,7 +53,7 @@ class TennisGameScore {
         this.gameScoreHeader.add(this.winner + " wins 1 point");
     }
 
-    private String toTennisScore(int score) {
+    String toTennisScore(int score) {
         switch(score) {
             case 1: return "15";
             case 2: return "30";
--- tennis_kata_referee/src/test/java/TennisGameScoreTest.java
+++ tennis_kata_referee/src/test/java/TennisGameScoreTest.java
@@ -68,4 +68,52 @@ public class TennisGameScoreTest {
         assertThat(tennisGameScore.getNadalScore()).isEqualTo(1);
     }
 
+    @Test
+    public void should_give_0_as_tennis_score_when_score_is_0() {
+        // Arrange
+        int score = 0;
+
+        // Act
+        String tennisScore = tennisGameScore.toTennisScore(score);
+
+        // Assert
+        assertThat(tennisScore).isEqualTo("0");
+    }
+
+    @Test
+    public void should_give_15_as_tennis_score_when_score_is_1() {
+        // Arrange
+        int score = 1;
+
+        // Act
+        String tennisScore = tennisGameScore.toTennisScore(score);
+
+        // Assert
+        assertThat(tennisScore).isEqualTo("15");
+    }
+
+    @Test
+    public void should_give_30_as_tennis_score_when_score_is_2() {
+        // Arrange
+        int score = 2;
+
+        // Act
+        String tennisScore = tennisGameScore.toTennisScore(score);
+
+        // Assert
+        assertThat(tennisScore).isEqualTo("30");
+    }
+
+    @Test
+    public void should_give_40_as_tennis_score_when_score_is_3() {
+        // Arrange
+        int score = 3;
+
+        // Act
+        String tennisScore = tennisGameScore.toTennisScore(score);
+
+        // Assert
+        assertThat(tennisScore).isEqualTo("40");
+    }
+
 }
